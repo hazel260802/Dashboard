ba0f6651db5236c4e40b43d5bdfe83e987fd7c94
hazel260802 <lehuyentrang260802@example.com>
2023-07-03 16:47:25 +0700

    Update

--- config.js
+++ config.js
@@ -1,18 +1,6 @@
 // config.js
 const config = {
-    db: {
-      host: '103.173.254.82',
-      user: 'dbhuan',
-      password: '0866444202',
-      port: '3306',
-      database: 'doandb',
-      pool: {
-        min: 1, 
-        max: 100, 
-        idleTimeoutMillis: 30000
-      },
-    },
-    ratingInterval: parseInt(60000),
-  };
+  ratingInterval: parseInt(60000),
+};
 
 module.exports = config;
